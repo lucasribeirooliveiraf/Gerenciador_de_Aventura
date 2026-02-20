34d33ad268543ff5f6480eafafa5cf1b94f8ed4a
Lucas Ribeiro <lucasjogos423@gmail.com>
2026-02-20 14:00:34 -0300

    Criação do sistema de bloco de notas

---
+++ script.js
@@ -0,0 +1,45 @@
+const elements = document.querySelectorAll(".btn")
+
+
+elements.forEach(element => {
+    element.addEventListener('click', () =>{
+        let command = element.dataset['element'];
+        if (command == "createLink" || command == "insertImage"){
+            let url = prompt("Enter the link here:", "http://");
+            document.execCommand(command, false, url);
+        }
+        
+        else if (command === "h1"){
+            document.execCommand('formatBlock', false, '<h1>');
+        }
+
+        else if (command === "p"){
+            document.execCommand('formatBlock', false, '<p>');
+        }
+
+        else{
+            document.execCommand(command, false, null);
+        }
+
+        
+    });
+});
+
+
+const texto = document.getElementById("texto")
+
+const palavras = document.getElementById("palavras");
+
+const caracteres = document.getElementById("caracteres");
+
+texto.addEventListener("input", () => {
+    let conteudo = texto.innerText.trim();
+    
+    // contar caracteres (com espaço)
+    caracteres.textContent = conteudo.length + " caracteres";
+
+
+    // contar palavras
+    let listaPalavras = conteudo.trim().split(/\s+/);
+    palavras.textContent = conteudo.trim() === "" ? 0 + " palavras" : listaPalavras.length + " palavras";
+});
